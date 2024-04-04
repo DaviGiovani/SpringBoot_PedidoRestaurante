15a0abab309fb3abc755c378615733aea747416d
Davi Giovani <113468321+DaviGiovani@users.noreply.github.com>
2024-04-04 16:06:26 -0300

    Delete projeto_Sistema Pedidos/demo directory

--- projeto_Sistema Pedidos/demo/src/main/java/repository/CozinheiroRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.Cozinheiro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CozinheiroRepository extends JpaRepository<Cozinheiro,Long> {
-}
--- projeto_Sistema Pedidos/demo/src/main/java/repository/GerenteRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import model.Gerente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GerenteRepository extends JpaRepository<Gerente, Long> {
-}
